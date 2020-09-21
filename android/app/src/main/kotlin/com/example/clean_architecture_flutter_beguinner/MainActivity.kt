240f1386105a552a4100456f133d74bd6757dabb
David Perez <davidpp124@hgmail.com>
2020-09-20 23:02:44 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/clean_architecture_flutter_beguinner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clean_architecture_flutter_beguinner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -0,0 +1,39 @@
+import 'package:clean_architecture_flutter_beguinner/core/usecases/usecase.dart';
+import 'package:clean_architecture_flutter_beguinner/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_flutter_beguinner/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:clean_architecture_flutter_beguinner/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_flutter_beguinner/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetRandomNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+  test('should get trivia for random number in the repository', () async {
+    //arrange
+    when(mockNumberTriviaRepository.getRandomNumberTrivia())
+        .thenAnswer((_) async => Right(tNumberTrivia));
+    // act
+    final result = await usecase(NoParams());
+    // assert
+    expect(result, Right(tNumberTrivia));
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
+  });
+
+  tearDown(() {
+    usecase = null;
+    mockNumberTriviaRepository = null;
+  });
+}
