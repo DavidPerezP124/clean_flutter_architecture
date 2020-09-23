d4f640df481503ec9f424a13acf6707b11394f6d
David Perez <davidpp124@hgmail.com>
2020-09-23 14:35:41 -0500

    bloc setup

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:clean_architecture_flutter_beguinner/core/presentation/util/input_converter.dart';
 import 'package:clean_architecture_flutter_beguinner/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_flutter_beguinner/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_flutter_beguinner/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
@@ -9,7 +12,17 @@ part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc();
+  final InputConverter inputConverter;
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+
+  NumberTriviaBloc(
+      {this.inputConverter,
+      this.getConcreteNumberTrivia,
+      this.getRandomNumberTrivia});
+
+  @override
+  NumberTriviaState get initialState => Empty();
 
   @override
   Stream<NumberTriviaState> mapEventToState(
@@ -17,8 +30,4 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   ) async* {
     // TODO: implement mapEventToState
   }
-
-  @override
-  // TODO: implement initialState
-  NumberTriviaState get initialState => throw UnimplementedError();
 }
--- test/core/fixtures/fixture_reader.dart
+++ test/core/fixtures/fixture_reader.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
 
 String fixture(String name) => File(
-        '/home/runner/work/clean_flutter_architecture/clean_flutter_architecture/test/core/fixtures/$name')
-    .readAsStringSync();
+    //   '/home/runner/work/clean_flutter_architecture/clean_flutter_architecture/test/core/fixtures/$name')
+    'test/core/fixtures/$name').readAsStringSync();
