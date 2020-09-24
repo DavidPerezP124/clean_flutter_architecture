4eac3ab19286b5b8fb32cb274e2f0e7fe757aaf0
David Perez <davidpp124@hgmail.com>
2020-09-23 23:07:13 -0500

    Added UI

--- test/core/fixtures/fixture_reader.dart
+++ test/core/fixtures/fixture_reader.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 String fixture(String name) => File(
         // change before pushing to github
-        //  '/home/runner/work/clean_flutter_architecture/clean_flutter_architecture/test/core/fixtures/$name')
-        'test/core/fixtures/$name')
+        '/home/runner/work/clean_flutter_architecture/clean_flutter_architecture/test/core/fixtures/$name')
+    //  'test/core/fixtures/$name')
     // keep this line
     .readAsStringSync();
